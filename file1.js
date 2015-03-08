ce70f228cf3477a20a0555fab53776af954db1ac
david-hammer <dhammer126@gmail.com>
2015-03-08 10:54:16 -0700

    make more concise / confusing

--- file1.js
+++ file1.js
@@ -1,10 +1,6 @@
 (function(){
   var fib = function(N) {
-    if (N <= 1){
-      return 1;
-    } else {
-      return N * fib(N-1);
-    }
+    return (N <= 1) ? 1 : N * fib(N-1);
   };
   console.log(fib(5));
 })()
